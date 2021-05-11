66595d27e8b019b7c4c2a35b0821455ba6c02de8
Kmilo9999 <k.diaz99@gmail.com>
2021-05-11 14:28:14 -0400

    fixed ident format.

--- libs/GLMLoader/GLMLoader.cpp
+++ libs/GLMLoader/GLMLoader.cpp
@@ -42,18 +42,18 @@
 /* GLMtriangle: Structure that defines a triangle in a model.
 */
 typedef struct _GLMtriangle {
-	GLuint vindices[3];			/* array of triangle vertex indices */	
-	GLuint nindices[3];			/* array of triangle normal indices */
-	GLuint txindices[3];			/* array of triangle vertex indices */
-	GLuint findex;			/* index of triangle facet normal */
+  GLuint vindices[3];			/* array of triangle vertex indices */
+  GLuint nindices[3];			/* array of triangle normal indices */
+  GLuint txindices[3];			/* array of triangle vertex indices */
+  GLuint findex;			/* index of triangle facet normal */
 } GLMtriangle;
 
 /* _GLMnode: general purpose node
 */
 typedef struct _GLMnode {
-	GLuint           index;
-	GLboolean        averaged;
-	struct _GLMnode* next;
+  GLuint           index;
+  GLboolean        averaged;
+  struct _GLMnode* next;
 } GLMnode;
 
 /* glmCross: compute the cross product of two vectors
@@ -65,11 +65,11 @@ typedef struct _GLMnode {
 static GLvoid
 glmCross(GLfloat* u, GLfloat* v, GLfloat* n)
 {
-	assert(u); assert(v); assert(n);
+  assert(u); assert(v); assert(n);
 
-	n[0] = u[1] * v[2] - u[2] * v[1];
-	n[1] = u[2] * v[0] - u[0] * v[2];
-	n[2] = u[0] * v[1] - u[1] * v[0];
+  n[0] = u[1] * v[2] - u[2] * v[1];
+  n[1] = u[2] * v[0] - u[0] * v[2];
+  n[2] = u[0] * v[1] - u[1] * v[0];
 }
 
 /* glmNormalize: normalize a vector
@@ -79,14 +79,14 @@ glmCross(GLfloat* u, GLfloat* v, GLfloat* n)
 static GLvoid
 glmNormalize(GLfloat* v)
 {
-	GLfloat l;
+  GLfloat l;
 
-	assert(v);
+  assert(v);
 
-	l = (GLfloat)sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-	v[0] /= l;
-	v[1] /= l;
-	v[2] /= l;
+  l = (GLfloat)sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+  v[0] /= l;
+  v[1] /= l;
+  v[2] /= l;
 }
 
 /* glmDot: compute the dot product of two vectors
@@ -97,16 +97,16 @@ glmNormalize(GLfloat* v)
 static GLfloat
 glmDot(GLfloat* u, GLfloat* v)
 {
-	assert(u); assert(v);
+  assert(u); assert(v);
 
-	return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+  return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
 }
 
 
 Model* GLMLoader::loadObjModel(std::string& filename)
 {
   tinyobj::ObjReaderConfig reader_config;
-	tinyobj::ObjReader reader;
+  tinyobj::ObjReader reader;
   if (!reader.ParseFromFile(filename, reader_config)) {
     if (!reader.Error().empty()) {
       std::cerr << "TinyObjReader: " << reader.Error();
@@ -122,18 +122,18 @@ Model* GLMLoader::loadObjModel(std::string& filename)
   auto& shapes = reader.GetShapes();
   auto& materials = reader.GetMaterials();
 
-	std::vector<float> vertices2 = attrib.GetVertices();
-	//std::vector<float> normals = attrib.normals;
-	std::vector<float> text = attrib.texcoords;
+  std::vector<float> vertices2 = attrib.GetVertices();
+  //std::vector<float> normals = attrib.normals;
+  std::vector<float> text = attrib.texcoords;
 
 
   std::vector<glm::vec3> vertices;
-	std::vector<glm::vec2> textcoords;
-	std::vector<glm::vec3> normals;
+  std::vector<glm::vec2> textcoords;
+  std::vector<glm::vec3> normals;
   std::vector<unsigned int> indices;
   std::unordered_map<glm::vec3, int> uniqueVertices;
-	std::unordered_map<glm::vec2, int> uniqueTextCoords;
-	std::unordered_map<glm::vec3, int> uniqueNormals;
+  std::unordered_map<glm::vec2, int> uniqueTextCoords;
+  std::unordered_map<glm::vec3, int> uniqueNormals;
 
   for (const auto& shape : shapes)
   {
@@ -144,46 +144,46 @@ Model* GLMLoader::loadObjModel(std::string& filename)
           attrib.vertices[3 * index.vertex_index + 1],
           attrib.vertices[3 * index.vertex_index + 2] };
 
-        glm::vec2 text_coord{
-         attrib.texcoords[2 * index.texcoord_index + 0],
-         attrib.texcoords[2 * index.texcoord_index + 1]};
-				
-        // Load normals per vertex
-        /* 
-        glm::vec3 normal{
-        attrib.texcoords[3 * index.normal_index + 0],
-        attrib.texcoords[3 * index.normal_index + 1],
-        attrib.texcoords[3 * index.normal_index + 2] };*/
+      glm::vec2 text_coord{
+       attrib.texcoords[2 * index.texcoord_index + 0],
+       attrib.texcoords[2 * index.texcoord_index + 1] };
+
+      // Load normals per vertex
+      /*
+      glm::vec3 normal{
+      attrib.texcoords[3 * index.normal_index + 0],
+      attrib.texcoords[3 * index.normal_index + 1],
+      attrib.texcoords[3 * index.normal_index + 2] };*/
 
       if (uniqueVertices.count(position) == 0)
       {
         uniqueVertices[position] = static_cast<int>(vertices.size());
-        vertices.push_back( position );
+        vertices.push_back(position);
       }
 
       if (uniqueTextCoords.count(text_coord) == 0)
       {
-				uniqueTextCoords[text_coord] = static_cast<int>(textcoords.size());
-				textcoords.push_back(text_coord);
+        uniqueTextCoords[text_coord] = static_cast<int>(textcoords.size());
+        textcoords.push_back(text_coord);
       }
-      
+
       // Load normals per vertex
-			/*
+      /*
       if (uniqueNormals.count(normal) == 0)
       {
-				uniqueNormals[normal] = static_cast<int>(normals.size());
-				normals.push_back(normal);
+        uniqueNormals[normal] = static_cast<int>(normals.size());
+        normals.push_back(normal);
       }*/
 
       indices.push_back(uniqueVertices[position]);
     }
   }
 
-	std::cout << "num vertices2 " << vertices2.size() << std::endl; 
-	std::cout << "num vertices " << vertices.size() << std::endl;
+  std::cout << "num vertices2 " << vertices2.size() << std::endl;
+  std::cout << "num vertices " << vertices.size() << std::endl;
   std::cout << "num normals " << normals.size() << std::endl;
   std::cout << "num text " << text.size() << std::endl;
-	std::cout << "num text2 " << textcoords.size() << std::endl;
+  std::cout << "num text2 " << textcoords.size() << std::endl;
 
 
   VertexBuffer* vertexObject = new VertexBuffer();
