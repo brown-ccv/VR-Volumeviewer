1b5cdf38fb9fee18d74da3a58523d9d273057637
Kmilo9999 <k.diaz99@gmail.com>
2021-08-04 00:50:45 -0400

    added glew and minvr modules

--- superbuild/macros.cmake
+++ superbuild/macros.cmake
@@ -20,6 +20,7 @@ macro(setup_subproject_path_vars _NAME)
   set(SUBPROJECT_SOURCE_PATH ${SUBPROJECT_NAME}/source)
   set(SUBPROJECT_STAMP_PATH ${SUBPROJECT_NAME}/stamp)
   set(SUBPROJECT_BUILD_PATH ${SUBPROJECT_NAME}/build)
+  
 endmacro()
 
 macro(build_git_subproject)
@@ -123,14 +124,62 @@ macro(build_glew_subproject)
   # Setup SUBPROJECT_* variables (containing paths) for this function
   setup_subproject_path_vars(${BUILD_SUBPROJECT_NAME})
 
+   message(STATUS "SUBPROJECT_INSTALL_PATH: ${SUBPROJECT_INSTALL_PATH}")
+
   # Build the actual subproject
   ExternalProject_Add(${SUBPROJECT_NAME}
     PREFIX ${SUBPROJECT_NAME}
     DOWNLOAD_DIR ${SUBPROJECT_NAME}
     STAMP_DIR ${SUBPROJECT_STAMP_PATH}
-    SOURCE_DIR ${SUBPROJECT_SOURCE_PATH}/build/cmake
+    SOURCE_DIR ${SUBPROJECT_SOURCE_PATH}
     BINARY_DIR ${SUBPROJECT_BUILD_PATH}
     URL   ${BUILD_SUBPROJECT_URL}
+    LIST_SEPARATOR | # Use the alternate list separator
+    CMAKE_ARGS
+	  -S ${CMAKE_CURRENT_SOURCE_DIR}/glew/source/build/cmake
+      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+      -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+      -DCMAKE_INSTALL_PREFIX:PATH=${SUBPROJECT_INSTALL_PATH}
+      -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
+      -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
+      -DCMAKE_INSTALL_DOCDIR=${CMAKE_INSTALL_DOCDIR}
+      -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
+      -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+      ${BUILD_SUBPROJECT_BUILD_ARGS}
+    BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
+    BUILD_ALWAYS OFF
+  )
+
+  if(BUILD_SUBPROJECT_DEPENDS_ON)
+    ExternalProject_Add_StepDependencies(${SUBPROJECT_NAME}
+      configure ${BUILD_SUBPROJECT_DEPENDS_ON}
+    )
+  endif()
+
+  # Place installed component on CMAKE_PREFIX_PATH for downstream consumption
+  append_cmake_prefix_path(${SUBPROJECT_INSTALL_PATH})
+endmacro()
+macro(build_glfw_subproject)
+  # See cmake_parse_arguments docs to see how args get parsed here:
+  #    https://cmake.org/cmake/help/latest/command/cmake_parse_arguments.html
+  set(oneValueArgs NAME URL)
+  set(multiValueArgs BUILD_ARGS DEPENDS_ON)
+  cmake_parse_arguments(BUILD_SUBPROJECT "" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN})
+
+  # Setup SUBPROJECT_* variables (containing paths) for this function
+  setup_subproject_path_vars(${BUILD_SUBPROJECT_NAME})
+
+  # Build the actual subproject
+  ExternalProject_Add(${SUBPROJECT_NAME}
+    PREFIX ${SUBPROJECT_NAME}
+    DOWNLOAD_DIR ${SUBPROJECT_NAME}
+    STAMP_DIR ${SUBPROJECT_STAMP_PATH}
+    SOURCE_DIR ${SUBPROJECT_SOURCE_PATH}
+    BINARY_DIR ${SUBPROJECT_BUILD_PATH}
+    GIT_REPOSITORY  ${BUILD_SUBPROJECT_URL}
+	GIT_TAG gpu-affinity
     LIST_SEPARATOR | # Use the alternate list separator
     CMAKE_ARGS
       -DCMAKE_BUILD_TYPE=Release
@@ -142,6 +191,9 @@ macro(build_glew_subproject)
       -DCMAKE_INSTALL_DOCDIR=${CMAKE_INSTALL_DOCDIR}
       -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
       -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+      -DGLFW_BUILD_EXAMPLES=OFF
+	  -DGLFW_BUILD_TESTS=OFF
+	  -DGLFW_BUILD_DOCS=OFF
       ${BUILD_SUBPROJECT_BUILD_ARGS}
     BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
     BUILD_ALWAYS OFF
