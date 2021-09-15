94aba0bdf840c0a96cd552e3aea8e8a6683d4fed
Camilo Diaz <k.diaz99@gmail.com>
2021-09-14 23:28:17 -0400

    Update execute_bat2exe_background.bat

--- superbuild/scripts/execute_bat2exe_background.bat
+++ superbuild/scripts/execute_bat2exe_background.bat
@@ -1,2 +1,2 @@
 echo %1
-superbuild\bat2exe\upload\bat2exe.exe /source:%1\superbuild\install_Windows\bin /target:%1\superbuild\install_Windows\bin /s /y
+superbuild\bat2exe\upload\bat2exe.exe /source:%1\superbuild /target:%1\superbuild /s /y
