1cc9c27f0888faa98432c3edf4d956211b6886f3
Kmilo9999 <k.diaz99@gmail.com>
2022-04-19 19:35:39 -0400

    add Choreograph to superbuild script

--- superbuild/macros.cmake
+++ superbuild/macros.cmake
@@ -33,7 +33,7 @@ macro(build_git_subproject)
 
   # Setup SUBPROJECT_* variables (containing paths) for this function
   setup_subproject_path_vars(${BUILD_SUBPROJECT_NAME})
-
+  
   # Build the actual subproject
   ExternalProject_Add(${SUBPROJECT_NAME}
     PREFIX ${SUBPROJECT_NAME}
@@ -68,6 +68,55 @@ macro(build_git_subproject)
   # Place installed component on CMAKE_PREFIX_PATH for downstream consumption
   append_cmake_prefix_path(${SUBPROJECT_INSTALL_PATH})
 endmacro()
+
+
+macro(build_choerograph_subproject)
+  # See cmake_parse_arguments docs to see how args get parsed here:
+  #    https://cmake.org/cmake/help/latest/command/cmake_parse_arguments.html
+  set(oneValueArgs NAME URL )
+  set(multiValueArgs BUILD_ARGS DEPENDS_ON)
+  cmake_parse_arguments(BUILD_SUBPROJECT "" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN})
+
+  # Setup SUBPROJECT_* variables (containing paths) for this function
+  setup_subproject_path_vars(${BUILD_SUBPROJECT_NAME})
+  
+  # Build the actual subproject
+  ExternalProject_Add(${SUBPROJECT_NAME}
+    PREFIX ${SUBPROJECT_NAME}
+    DOWNLOAD_DIR ${SUBPROJECT_NAME}
+    STAMP_DIR ${SUBPROJECT_STAMP_PATH}
+    SOURCE_DIR ${SUBPROJECT_SOURCE_PATH}
+    BINARY_DIR ${SUBPROJECT_BUILD_PATH}
+    GIT_REPOSITORY  ${BUILD_SUBPROJECT_URL}
+	GIT_TAG stable
+    LIST_SEPARATOR | # Use the alternate list separator
+    CMAKE_ARGS
+	  -S ${CMAKE_CURRENT_SOURCE_DIR}/Choreograph/source/src
+      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+      -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+      -DCMAKE_INSTALL_PREFIX:PATH=${SUBPROJECT_INSTALL_PATH}
+      -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
+      -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
+      -DCMAKE_INSTALL_DOCDIR=${CMAKE_INSTALL_DOCDIR}
+      -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
+      -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+      ${BUILD_SUBPROJECT_BUILD_ARGS}
+    BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
+    BUILD_ALWAYS OFF
+  )
+
+  if(BUILD_SUBPROJECT_DEPENDS_ON)
+    ExternalProject_Add_StepDependencies(${SUBPROJECT_NAME}
+      configure ${BUILD_SUBPROJECT_DEPENDS_ON}
+    )
+  endif()
+
+  # Place installed component on CMAKE_PREFIX_PATH for downstream consumption
+  append_cmake_prefix_path(${SUBPROJECT_INSTALL_PATH})
+endmacro()
+
 
 macro(build_minvr_subproject)
   # See cmake_parse_arguments docs to see how args get parsed here:
@@ -326,7 +375,7 @@ macro(build_glm_subproject)
     SOURCE_DIR ${SUBPROJECT_SOURCE_PATH}
     BINARY_DIR ${SUBPROJECT_BUILD_PATH}
     GIT_REPOSITORY  ${BUILD_SUBPROJECT_URL}
-	  GIT_TAG ${BUILD_SUBPROJECT_TAG}
+	GIT_TAG ${BUILD_SUBPROJECT_TAG}
     LIST_SEPARATOR | # Use the alternate list separator
     CMAKE_ARGS
 
